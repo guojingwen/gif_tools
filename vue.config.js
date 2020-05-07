6b318b610d28662750a6a5f981bacb7446214030
guojw <2315162186@qq.com>
2020-05-07 12:59:01 +0800

    配置

--- vue.config.js
+++ vue.config.js
@@ -1,4 +1,16 @@
+const CopyWebpackPlugin = require('copy-webpack-plugin')
+
 module.exports = {
   filenameHashing: false,
-  configureWebpack: {}
+  publicPath: process.env.NODE_ENV === 'production'
+    ? '/gif_demo/'
+    : '/',
+  configureWebpack: {
+    plugins: [
+      new CopyWebpackPlugin([{
+        from: 'src/static',
+        to: 'static'
+      }])
+    ]
+  }
 }
